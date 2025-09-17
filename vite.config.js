063b8771e79f838db801414ed525ab4380f5e15a
chitintim <tim.chiutin.lam@gmail.com>
2025-09-17 17:32:49 +0100

    Update service worker to force cache refresh and auto-update

--- vite.config.js
+++ vite.config.js
@@ -29,6 +29,9 @@ export default defineConfig({
         ]
       },
       workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,json,woff2}'],
         runtimeCaching: [
           {
